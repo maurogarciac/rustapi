67721cf56148a32804dbafe0ba3790aedd05771f
maurogarciac <maurogarciacoto@gmail.com>
2023-06-01 17:22:41 -0300

    created getter for 'blog posts'

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rustapi
 An api to practice rocket.rs
+
+
+# why this stack?
+## Rocket 
+it handles the api crud requests  
+is based on Tokio...
+## Diesel
+works as a query builder for the db
+## Postgres
+is a db
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,62 @@
 #[macro_use] extern crate rocket;
+
 use rocket_dyn_templates::{Template, context};
+use rocket::{serde::json::{serde_json::{json, Value}, Json}, http::Status};
+use rocket::serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Test  {
+    id: i32,
+    title: String,
+    body: String,
+}
 
 #[get("/")]
-fn hello() -> Template {
+fn index() -> Template {
     Template::render("index", context!{
         title: "Test"
     })
 }
 
-#[get("/hi")]
-fn test() -> &'static str {
-    "how did you get here"
+#[get("/json")]
+fn json() -> Value {
+    json!({
+        "message" : "how did you get here"
+    })
+}
+
+#[get("/test")]
+fn test() -> Json<Test> {
+    Json(
+        Test{
+            id: 1,
+            title: "Test 1".to_string(),
+            body: "Lorem ipsum etc...".to_string(),
+        }
+    )
+}
+#[get("/posts/<name>")]
+fn get_blog_posts(name:&str) -> Result<Template, Status> {
+    let s_name : String = name.to_string(); 
+    let template : Template = Template::render(s_name, context! {
+        title: format!("Post {}", name)
+    });
+    return Ok(template);
+    /*
+    if let Some(template) = template {
+        return Ok(template);
+    }else {
+        return Err(Status::NotFound);
+    }
+    */
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello, test])
-    .attach(Template::fairing())
+    rocket::build()
+        .mount("/", routes![index, json, test, get_blog_posts])
+        .attach(Template::fairing())
 }
+
+
