9fd6b238f9b1f0f89392022d8b3e78240b9280b2
maurogarciac <maurogarciacoto@gmail.com>
2023-06-01 11:19:32 -0300

    added handlebars

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 #[macro_use] extern crate rocket;
+use rocket_dyn_templates::{Template, context};
 
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+fn hello() -> Template {
+    Template::render("index", context!{
+        title: "Test"
+    })
+}
+
+#[get("/hi")]
+fn test() -> &'static str {
+    "how did you get here"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![hello, test])
+    .attach(Template::fairing())
 }
